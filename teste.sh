b4c0d5bdf95f5da02f2fa1261211a4c7d5807246
Henrique Nogueira <42853926+henriquenogueira@users.noreply.github.com>
2020-08-19 13:29:10 -0300

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -1,1 +1,3 @@
-terraform init -env accessKey= $accessKey1 -env secretKey= $accessSecret1 /tmp/terraformLab/
+terraform init /tmp/terraformLab/
+terraform plan -env accessKey= $accessKey1 -env secretKey= $accessSecret1 /tmp/terraformLab/
+terraform apply -auto-approve /tmp/terraformLab
